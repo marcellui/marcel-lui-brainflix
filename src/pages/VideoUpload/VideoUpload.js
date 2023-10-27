bb6d1a998c9ca6d9b3f1c4fba2c728d5494b53b2
unknown <marcellui.ca@gmail.comgit config --global user.name marcellui>
2023-10-27 17:20:58 -0400

    finish videoupload page and change css

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -1,36 +1,47 @@
-import "./VideoUpload.scss"
-import thumbnail from "../../assets/Images/Upload-video-preview.jpg"
-import uploadIcon from "../../assets/Icons/upload.svg"
+import "./VideoUpload.scss";
+import thumbnail from "../../assets/Images/Upload-video-preview.jpg";
+import uploadIcon from "../../assets/Icons/upload.svg";
 
 function VideoUpload() {
   return (
     <>
       <div className="video__hero">
         <h1>Upload Video</h1>
-        <p>VIDEO THUMBNAIL</p>
-        <img
-          src= {thumbnail} 
-          alt="upload"
-          className="video__image"
-        ></img>
-        <form>
-          <label>TITLE YOUR VIDEO</label>
-          <br></br>
-          <input type="text" placeholder="Add a title to your video"></input>
-          <br></br>
-          <label>ADD A VIDEO DESCRIPTION</label>
-          <br></br>
-          <textarea
-            type="text"
-            placeholder="Add a description to your video"
-          ></textarea>
-        </form>
+        <div className="video__flex-container">
+          <div className="video__img-container">
+            <p className="video__title3">VIDEO THUMBNAIL</p>
+            <img src={thumbnail} alt="upload" className="video__image"></img>
+          </div>
+          <div className="video__form">
+            <form>
+              <label className="video__title3">TITLE YOUR VIDEO</label>
+              <br></br>
+              <input
+                type="text"
+                placeholder="Add a title to your video"
+                className="video__text-1"
+              ></input>
+              <br></br>
+              <label className="video__title3">ADD A VIDEO DESCRIPTION</label>
+              <br></br>
+              <textarea
+                type="text"
+                placeholder="Add a description to your video"
+                className="video__text-2"
+              ></textarea>
+            </form>
+          </div>
+        </div>
       </div>
-      <div>
-        <img src={uploadIcon} alt="upload icon"></img>
-        <p>PUBLISH</p>
+      <div className="video__container1">
+        <div className="video__button">
+          <img src={uploadIcon} alt="upload icon" className="video__icon"></img>
+          <p className="video__button-publish">PUBLISH</p>
+        </div>
+        <a href="/">
+            <p className="video__button-cancel">CANCEL</p>
+        </a>
       </div>
-      <p>CANCEL</p>
     </>
   );
 }
